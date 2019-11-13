1bddc6c263a71e69580479a6f3c0f0840f3b94fb
Marcela Beduschi <marcelabbc07@gmail.com>
2019-11-13 19:17:11 -0300

    Aula6

--- Aula6/exercicio3.py
+++ Aula6/exercicio3.py
@@ -1,16 +1,16 @@
 lista_alunos=[]
 lista_notas=[]
-for i in range(0,10):
+for i in range(0,10,1):
     lista_alunos.append(input('Digite seu nome:'))
-    for x in range(0,10):
+    for i in range(0,1):
         lista_notas.append(float(input('Digite a primeira nota:')))
         lista_notas.append(float(input('Digite a segunda nota:')))
         lista_notas.append(float(input('Digite a terceira nota:')))
         lista_notas.append(float(input('Digite a quarta nota:')))
-        media=(lista_notas[x])/4
-        if(media>=7.0):
-            print('Aprovado')
-        else:
-            print('Reprovado')
-for i in range(0,10):
-    print(f'{i}-Nome:{lista_alunos}\nMedia:{media}')
+        media=(lista_notas[0]+lista_notas[1]+lista_notas[2]+lista_notas[3])/4
+        for i in lista_alunos:
+            print(f'Nome:{[i]}\nMedia:{media}')
+            if(media>=7.0):
+                print('Aprovado')
+            else:
+                print('Reprovado')

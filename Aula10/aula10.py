2697c737722da6880d5393f68b76fa377333b88e
Marcela Beduschi <marcelabbc07@gmail.com>
2019-11-20 11:54:33 -0300

    Aula10

---
+++ Aula10/aula10.py
@@ -0,0 +1,21 @@
+nome_pagina='Calculadora'
+from flask import Flask, render_template, request
+from calculos import soma,sub,mult,div_int,div_fra,resto,raiz
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('aula10.html',titulo=nome_pagina) 
+@app.route('/calcular')
+def calcular():
+    n1=int(request.args['n1'])
+    n2=int(request.args['n2'])
+    r_soma=soma(n1,n2)
+    r_sub=sub(n1,n2)
+    r_mult=mult(n1,n2)
+    r_div_int=div_int(n1,n2)
+    r_div_fra=div_fra(n1,n2)
+    r_resto=resto(n1,n2)
+    r_raiz=raiz(n1,n2)
+    resultados={'soma':r_soma,'sub':r_sub,'mult':r_mult,'div_int':r_div_int,'div_fra':r_div_fra,'resto':r_resto,'raiz':r_raiz}
+    return render_template('resultado.html',n1=n1,n2=n2,print(f"{resultados['soma']},{resultados['sub']},{resultados['mult']},{resultados['div_int']},{resultados['div_fra']},{resultados['resto']},{resultados['raiz']}"))
+app.run()
---
+++ Aula10/calculos.py
@@ -0,0 +1,21 @@
+def soma(n1,n2):
+    soma=n1+n2
+    return soma
+def sub(n1,n2):
+    sub=n1-n2
+    return sub
+def mult(n1,n2):
+    mult=n1*n2
+    return mult
+def div_int(n1,n2):
+    div_int=n1//n2
+    return div_int
+def div_fra(n1,n2):
+    div_fra=n1/n2
+    return div_fra
+def resto(n1,n2):
+    resto=n1%n2
+    return resto
+def raiz(n1,n2):
+    raiz=n1**(1/n2)
+    return raiz

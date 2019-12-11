3e5d3bbf56e3347c6330e2a9900da48d9a61dc68
Marcela Beduschi <marcelabbc07@gmail.com>
2019-12-11 08:27:08 -0300

    Aula24

--- Aula23/exercicio2.py
+++ Aula23/exercicio2.py
@@ -13,3 +13,8 @@ dadobruto = '1;Arnaldo;23;m;alexcabeludo2@hotmail.com;014908648117'
 # código cliente (inteiro), nome, idade (inteiro), sexo, email, telefone
 # convertendo os valores de string para inteiros quando necessários. 
 # (Faça da forma que vocês conseguirem! O importante é o resultado e não como chegaram nele!)
+# 5) Crie um metodo salvar que pegue os seguintes dados do cliente e salve em um arquivo. 
+# código cliente (inteiro), nome, idade (inteiro), sexo, email, telefone
+# 6) crie um metodo que possa atualizar os dados do cliente (código cliente (inteiro), 
+# nome, idade (inteiro), sexo, email, telefone). Este metodo deverá alterar tambem o dado bruto para
+# que na hora de salvar o dado num arquivo, o mesmo não estaja desatualizado.
